4bfa68e286e7eceb7e339eefbbdb6136ad773831
GArthTex66 <garthur1998@gmail.com>
2024-03-16 23:44:50 -0300

    Adicina rotas

--- src/app/components/table-notas/table-notas.component.ts
+++ src/app/components/table-notas/table-notas.component.ts
@@ -1,31 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NotasService } from 'src/app/shared/service/notas.service';
-
+import { Observable } from 'rxjs';
+import {mergeMap} from 'rxjs/operators';
 @Component({
   selector: 'app-table-notas',
   templateUrl: './table-notas.component.html',
   styleUrls: ['./table-notas.component.css']
 })
 export class TableNotasComponent implements OnInit {
-
+ 
   notas = [];
+  obs:Observable<[]> ;
 
   constructor(private notasService:NotasService, private route:ActivatedRoute ) {}
 
   ngOnInit(): void {
-    this.route.queryParamMap.subscribe((params) => {
+    this.obs = this.route.queryParamMap.pipe(params => {
       console.log(params['params']);
+      if(params['params']?.filter === 'estado')
+        return this.notasService.getNotasPorEstado(params['params'].data);
+      else if(params['params']?.filter === 'data')
+        return this.notasService.getNotasPorData(params['params'].data);
+      else
+      return this.notasService.getNotas();
     });
-    this.getNotas();
-  }
 
-  getNotas() {
-    this.notasService.getNotas().subscribe((data) => {
+    this.obs.subscribe((data) => {
       this.notas = data;
       console.log(this.notas);
     });
   }
-
 
 }
--- src/app/shared/service/notas.service.ts
+++ src/app/shared/service/notas.service.ts
@@ -6,9 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class NotasService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }  
+
+  //Falta mudar endpoints para o backend e corrigit imprevistos
 
   getNotas() {
+    return this.http.get<any>('assets/notas-list.json');
+  }
+
+  getNotasPorEstado(estado: string) {
+    return this.http.get<any>('assets/notas-list.json');
+  }
+
+  getNotasPorData(data: string) {
     return this.http.get<any>('assets/notas-list.json');
   }
 
